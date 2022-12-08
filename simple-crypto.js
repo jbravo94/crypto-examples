a1323a431d8b673b514a35ed14cd267887a1890b
Johannes Heinzl <jh.heinzl@gmail.com>
2022-12-08 01:05:16 +0100

    Init

---
+++ simple-crypto.js
@@ -0,0 +1,37 @@
+import CryptoJS from "crypto-js";
+
+const password = "123dddddd";
+const secret = "test";
+
+const encrypt = (details) => {
+  // Create the main encryption.
+  const encrypted = CryptoJS.AES.encrypt(details.secret, details.password, {
+    iv: details.iv,
+  });
+
+  // Crypto JS Automatically gives us the values we need.
+  const { iv, key, ct } = encrypted;
+
+  // IV and Key need to be Hex
+  const ivHex = CryptoJS.enc.Hex.stringify(iv);
+  const keyHex = CryptoJS.enc.Hex.stringify(key);
+
+  // Convert encrypted to a string.
+  const base64Secret = encrypted.toString();
+
+  return { base64Secret, ivHex, keyHex };
+};
+
+const data = encrypt({ secret: secret, password: password });
+
+console.log(
+  `echo ${data.base64Secret} | openssl enc -base64 -d | openssl enc -d -aes-256-cbc -K ${data.keyHex} -iv ${data.ivHex} -k ${password}`
+);
+
+//echo U2FsdGVkX1+wL7+vQSU7DfXGO5DXoqkfu0y4mTyRAqs= | openssl enc -base64 -d | openssl enc -d -aes-256-cbc -K d8a26e4b184264c6166691eeba0afa3327119c417efbaccfcbca7a762c56a863 -iv 1bd381d5d8ddb71296e042b6bdebde62 -k 123dddddd
+
+const decrypted = CryptoJS.AES.decrypt(data.base64Secret, password);
+
+const str = decrypted.toString(CryptoJS.enc.Utf8);
+
+console.log(str);
---
+++ verbose-crypto.js
@@ -0,0 +1,59 @@
+import CryptoJS from "crypto-js";
+
+var message = "SuperSecret!!";
+var password = "Password01";
+
+var getKeyAndIV = function (password) {
+  var keyBitLength = 256;
+  var ivBitLength = 128;
+  var iterations = 234;
+
+  var bytesInSalt = 128 / 8;
+  var salt = CryptoJS.lib.WordArray.random(bytesInSalt);
+
+  var iv128Bits = CryptoJS.PBKDF2(password, salt, {
+    keySize: 128 / 32,
+    iterations: iterations,
+  });
+  var key256Bits = CryptoJS.PBKDF2(password, salt, {
+    keySize: 256 / 32,
+    iterations: iterations,
+  });
+
+  return {
+    iv: iv128Bits,
+    key: key256Bits,
+  };
+};
+
+var skey = getKeyAndIV(password);
+
+var data = CryptoJS.AES.encrypt(message, skey.key, { iv: skey.iv });
+
+const { iv, key, ct } = data;
+
+const ivHex = CryptoJS.enc.Hex.stringify(iv);
+const keyHex = CryptoJS.enc.Hex.stringify(key);
+const base64Secret = data.toString();
+
+console.log(
+  `echo ${base64Secret} | openssl enc -base64 -d | openssl enc -d -aes-256-cbc -K ${keyHex} -iv ${ivHex} -k ${password} -nosalt`
+);
+//echo mTbfrvvvGVHY+LCZfzSeqg== | openssl enc -base64 -d | openssl enc -d -aes-256-cbc -K 31c3f03aa212477e314973b15259dec6ee094c6607772a5dfc6cc8a75a068f07 -iv 31c3f03aa212477e314973b15259dec6 -k Password01 -nosalt
+
+var dc = data.ciphertext.toString(CryptoJS.enc.Base64);
+var dk = data.key.toString(CryptoJS.enc.Base64);
+var di = data.iv.toString(CryptoJS.enc.Base64);
+
+var bciphertext = CryptoJS.enc.Base64.parse(dc);
+var bkey = CryptoJS.enc.Base64.parse(dk);
+var biv = CryptoJS.enc.Base64.parse(di);
+
+var params = {
+  ciphertext: bciphertext,
+  salt: "",
+};
+
+var clearText = CryptoJS.AES.decrypt(params, bkey, { iv: biv });
+
+console.log(clearText.toString(CryptoJS.enc.Utf8));
